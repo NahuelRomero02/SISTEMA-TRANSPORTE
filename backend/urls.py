06fc2110ab5ac14116f26b143ab11233c93fa707
Agustin <juanarquipa6675@gmail.com>
2025-06-11 11:58:18 -0300

    Subiendo ultimos cambios.

--- backend/urls.py
+++ backend/urls.py
@@ -17,7 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cargas.urls')),
+    path('api-auth/', include('rest_framework.urls')),  # <--- Agrega esta línea
+    path('api/token-auth/', obtain_auth_token),  # <--- Endpoint para login por token
 ]
---
+++ docs.md
@@ -0,0 +1,7 @@
+# Datos
+okarol-ruta68
+3YpGbHeuKqet0e28VMBa
+8020
+
+ruta68-ssh
+tXodW6Ft9ZgxpHl7tSQp
