d17be29b5533d2e24e30e92f3e2ab317477fb625
Agustin <juanarquipa6675@gmail.com>
2025-06-21 03:59:55 -0300

    configuiracion de cors

--- backend/settings.py
+++ backend/settings.py
@@ -57,6 +57,7 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:8080",
+    "https://ruta68.nubelon.com",
 ]
 
 CORS_ALLOW_CREDENTIALS = True  # ✅ NECESARIO para cookies/sesiones
